a78858d5d7eea46c0b08bfa9a88ad9103da05856
Novyk Roman <novykroman12@gmail.com>
2026-07-02 12:19:04 +0300

    add months
    Updated max and min calculations to include month names.

--- Homework 3/Homework 3.py
+++ Homework 3/Homework 3.py
@@ -1,11 +1,12 @@
 input = [int(x) for x in input("Введіть кількість місячних опадів: ").split()]
+months = ["січень", "лютий", "березень", "квітень", "травень", "червень", "липень", "серпень", "вересень", "жовтень", "листопад", "грудень"]
 
 
 def main ():
     suma = sum(input)
     mid = suma / len(input)
-    maxi = max(input)
-    mini = min(input)
+    maxi = str(max(input)) + " - " + months[input.index(max(input))]
+    mini = str(min(input)) + " - " + months[input.index(min(input))]
 
     print("Сума опадів:", suma)
     print("Середнє значення опадів:", mid) 
